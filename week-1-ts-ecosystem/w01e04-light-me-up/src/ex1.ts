e18c2ef856da70059b0bf14fab21292b769d4c41
drazek <kamil.drazkiewicz@dryg.pl>
2022-09-26 20:45:24 +0200

    /solutions/week1/e04

---
+++ week-1-ts-ecosystem/w01e04-light-me-up/src/ex1.ts
@@ -0,0 +1,24 @@
+import LightBulb from "./LightBulb";
+import PowerSource from "./PowerSource";
+
+console.log("zadanie 1")
+const powerSource = new PowerSource();
+const lightBulb1 = new LightBulb(powerSource);
+const lightBulb2 = new LightBulb(powerSource);
+const lightBulb3 = new LightBulb(powerSource);
+const lightBulb4 = new LightBulb(powerSource);
+const lightBulb6 = new LightBulb(powerSource);
+const lightBulb5 = new LightBulb(powerSource)
+lightBulb1.consumption()
+lightBulb2.consumption()
+lightBulb3.consumption()
+lightBulb4.consumption()
+lightBulb5.consumption()
+lightBulb6.consumption()
+
+
+
+
+
+
+
