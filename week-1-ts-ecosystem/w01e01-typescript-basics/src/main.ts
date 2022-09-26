33ff0897642e646a49b69d22c642b8240fc762b0
Daniel Noworyta <daniel.noworyta@gmail.com>
2022-09-26 06:39:52 +0200

    Refactoring
    - get rid of unnecessary spaces

--- week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
@@ -1,7 +1,5 @@
 import user from "./store/Users.store";
 import {hasAddress, hasGivenAge, isAdult} from "./utils/users";
 
-
-
 console.log(`User ${user.name} is ${isAdult(user) ? 'adult' : 'minor'}`)
 console.log(`and has${hasAddress(user) ? '' : ' no'} address`)
