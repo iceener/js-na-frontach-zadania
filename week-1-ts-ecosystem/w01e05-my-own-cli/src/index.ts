04bee5efeb4791e0793a5d81a901565eae4df206
DragoBJJ <j.pawelski.it@gmail.com>
2022-09-28 19:21:18 +0200

    unused line

--- week-1-ts-ecosystem/w01e05-my-own-cli/src/index.ts
+++ week-1-ts-ecosystem/w01e05-my-own-cli/src/index.ts
@@ -4,14 +4,12 @@ import { questionName} from "./data";
 const clear = require('clear');
 const prompts = require('prompts')
 
-
 export const lets_play_the_game = async () => {
     clear()
     textMessage(`Welcome in Drago CLI `)
     const {Name} = await prompts(questionName,{onSubmit})
     await Avada_Kedavra(Name)
 }
-
 lets_play_the_game().then((res:any) => {
     if(res) console.log(res)
 }).catch((err) => console.log(err))
